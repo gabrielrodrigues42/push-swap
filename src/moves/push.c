08e548560c523173581d020295c7f8847e41c423
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2022-02-11 22:08:11 -0300

    fix: remove not expected protection verification

--- src/moves/push.c
+++ src/moves/push.c
@@ -6,30 +6,29 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 20:38:57 by gandrade          #+#    #+#             */
-/*   Updated: 2022/02/11 21:19:58 by gandrade         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:05:26 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	push_move(t_lst2c **head_x, t_lst2c **head_y)
+static void	push_move(t_lst2c **head_x, t_lst2c **head_y)
 {
 	if (!*head_x)
-		return (0);
+		return ;
 	ft_lst2c_add_front(head_y, ft_lst2c_pop_node(head_x, *head_x));
-	return (1);
 }
 
 void	push(t_stacks *stack, char name)
 {
 	if (name == 'a')
 	{
-		if (push_move(&stack->b, &stack->a))
-			ft_putendl_fd("pa", 1);
+		push_move(&stack->b, &stack->a);
+		ft_putendl_fd("pa", 1);
 	}
-	else
+	if (name == 'b')
 	{
-		if (push_move(&stack->a, &stack->b))
-			ft_putendl_fd("pb", 1);
+		push_move(&stack->a, &stack->b);
+		ft_putendl_fd("pb", 1);
 	}
 }

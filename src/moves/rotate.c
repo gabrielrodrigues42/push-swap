2048397b0d5dbbf88da4a0cc3345adf00a5a4794
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2022-02-11 22:19:21 -0300

    feat: rotate_move

--- src/moves/rotate.c
+++ src/moves/rotate.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 15:47:25 by gandrade          #+#    #+#             */
-/*   Updated: 2022/02/05 15:48:10 by gandrade         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:18:20 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 static void	rotate_move(t_lst2c **head)
 {
-
+	if (!*head || *head == (*head)->next)
+		return ;
+	*head = (*head)->next;
 }
 
 void	rotate(t_stacks *stack, char name)
@@ -24,12 +26,12 @@ void	rotate(t_stacks *stack, char name)
 		rotate_move(&stack->a);
 		ft_putendl_fd("ra", 1);
 	}
-	else if (name == 'b')
+	if (name == 'b')
 	{
 		rotate_move(&stack->b);
 		ft_putendl_fd("rb", 1);
 	}
-	else
+	if (name == 'r')
 	{
 		rotate_move(&stack->a);
 		rotate_move(&stack->b);

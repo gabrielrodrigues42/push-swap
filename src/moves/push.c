8338faaf59aa62e7e27001d117e11e41c31eed56
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2022-02-11 21:20:23 -0300

    feat: push_move

--- src/moves/push.c
+++ src/moves/push.c
@@ -6,27 +6,30 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 20:38:57 by gandrade          #+#    #+#             */
-/*   Updated: 2022/02/05 15:39:07 by gandrade         ###   ########.fr       */
+/*   Updated: 2022/02/11 21:19:58 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	push_move(t_lst2c **head_x, t_lst2c **head_y)
+static int	push_move(t_lst2c **head_x, t_lst2c **head_y)
 {
-	
+	if (!*head_x)
+		return (0);
+	ft_lst2c_add_front(head_y, ft_lst2c_pop_node(head_x, *head_x));
+	return (1);
 }
 
 void	push(t_stacks *stack, char name)
 {
 	if (name == 'a')
 	{
-		push_move(&stack->b, &stack->a);
-		ft_putendl_fd("pa", 1);
+		if (push_move(&stack->b, &stack->a))
+			ft_putendl_fd("pa", 1);
 	}
 	else
 	{
-		push_move(&stack->a, &stack->b);
-		ft_putendl_fd("pb", 1);
+		if (push_move(&stack->a, &stack->b))
+			ft_putendl_fd("pb", 1);
 	}
 }

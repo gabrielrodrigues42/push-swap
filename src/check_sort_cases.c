4dd49a6b15a9ba90a86302f8804af05a37c20cea
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2022-02-12 23:09:29 -0300

    fix: check_sort_case

--- src/check_sort_cases.c
+++ src/check_sort_cases.c
@@ -6,49 +6,25 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 13:23:42 by gandrade          #+#    #+#             */
-/*   Updated: 2022/02/12 23:03:05 by gandrade         ###   ########.fr       */
+/*   Updated: 2022/02/12 23:07:21 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
 static int	stack_is_sorted(t_lst2c *head);
 static int	stack_is_rev_sorted(t_lst2c *head);
 static void	sort_two(t_stacks *stack);
 
-int	list_size(t_lst2c *head)
-{
-	t_lst2c	*temp;
-	int		size;
-
-	size = 0;
-	temp = head;
-	while (temp->next != head)
-	{
-		temp = temp->next;
-		size++;
-	}
-	size++;
-	return (size);
-}
-
 void	check_sort_cases(t_stacks *stack)
 {
-	int	size = list_size(stack->a);
-	printf("%d\n", size);
+	int	size = ft_lst2c_size(stack->a);
 	if (size == 1 || stack_is_sorted(stack->a))
 		return ;
 	else if (size == 2)
 		sort_two(stack);
 	else
 		return ;
-	// if (stack->a == stack->a->next || stack_is_sorted(stack->a))
-	// 	return ;
-	// else if (stack->a == stack->a->next->next)
-	// 	sort_two(stack);
-	// else
-	// 	return ;
 }
 
 static int	stack_is_sorted(t_lst2c *head)

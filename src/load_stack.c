75fea25ea38f738f04e331c667927566b84f9a55
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2022-02-12 21:54:05 -0300

    remove printf from load stack

--- src/load_stack.c
+++ src/load_stack.c
@@ -6,25 +6,11 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 13:24:43 by gandrade          #+#    #+#             */
-/*   Updated: 2022/02/06 13:32:54 by gandrade         ###   ########.fr       */
+/*   Updated: 2022/02/12 21:53:42 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h> //REMOVE
-
-static void	print_list(t_lst2c *head) //REMOVE
-{
-	t_lst2c	*temp;
-
-	temp = head;
-	while (temp->next != head)
-	{
-		printf("%d\n", temp->data);
-		temp = temp->next;
-	}
-	printf("%d\n", temp->data);
-}
 
 void	load_values_to_stack_a(char **args, t_stacks *stack)
 {
@@ -38,6 +24,5 @@ void	load_values_to_stack_a(char **args, t_stacks *stack)
 		ft_lst2c_add_back(&stack->a, ft_lst2c_new(data));
 		i++;
 	}
-	print_list(stack->a); //REMOVE
 	ft_strclear2(args);
 }
